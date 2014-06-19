cf1efd00863585a926bce999a34d3a6023c758ef
Santiago Mota <santiago_mota@yahoo.es>
2014-06-19 22:10:38 +0200

    PCA

--- Practical_Machine_Learning_Project1.R
+++ Practical_Machine_Learning_Project1.R
@@ -62,37 +62,37 @@ training$user_name      <- as.factor(training$user_name)
 training$new_window     <- factor(training$new_window, labels=c("no", "yes"), 
                                   levels=c("no", "yes"))
 # training$cvtd_timestamp <- as.POSIXct(strptime(training$cvtd_timestamp, 
-#                                               "%d/%m/%Y %H:%M"))
+#                                                "%d/%m/%Y %H:%M"))
 
 testing$user_name      <- as.factor(testing$user_name)
 testing$new_window     <- factor(testing$new_window, labels=c("no", "yes"), 
                                  levels=c("no", "yes"))
 # testing$cvtd_timestamp <- as.POSIXct(strptime(testing$cvtd_timestamp, 
-#                                                "%d/%m/%Y %H:%M"))
+#                                               "%d/%m/%Y %H:%M"))
 
 classes1 <- sapply(training[1,], class)
 table(classes1)
 classes2 <- sapply(testing[1,], class)
 table(classes2)
 
-for (i in 1:34) {
-      print(classes_character[i])
-      print(table(training[, classes_character[i]]))
-}
-
-for (i in 2:34) {
-      training[, classes_character[i]][training[, classes_character[i]]==""] <- NA
-      training[, classes_character[i]][training[, classes_character[i]]=="#DIV/0!"] <- Inf
-      training[, classes_character[i]] <- as.numeric(as.character(training[, classes_character[i]]))
-      print(classes_character[i])
-      print(table(training[, classes_character[i]]))
-      print(class(training[, classes_character[i]]))
-}
-
 classes1[classes1=="character"]
 names(classes1[classes1=="character"])
 classes_character <- names(classes1[classes1=="character"])
 summary(training[, classes_character])
+
+for (i in 1:34) {
+  print(classes_character[i])
+  print(table(training[, classes_character[i]]))
+}
+
+for (i in 2:34) {
+  training[, classes_character[i]][training[, classes_character[i]]==""] <- NA
+  training[, classes_character[i]][training[, classes_character[i]]=="#DIV/0!"] <- Inf
+  training[, classes_character[i]] <- as.numeric(as.character(training[, classes_character[i]]))
+  print(classes_character[i])
+  print(table(training[, classes_character[i]]))
+  print(class(training[, classes_character[i]]))
+}
 
 sapply(training[, classes_character], as.numeric)
 classes1 <- sapply(training[1,], class)
@@ -119,16 +119,16 @@ classes2_logical <- names(classes2[classes2=="logical"])
 summary(testing[, classes2_logical])
 
 for (i in 1:100) {
-      print(classes2_logical[i])
-      print(table(testing[, classes2_logical[i]]))
+  print(classes2_logical[i])
+  print(table(testing[, classes2_logical[i]]))
 }
 
 for (i in 1:100) {
-      # testing[, classes2_logical[i]] <- as.numeric(as.character(testing[, classes2_logical[i]]))
-      testing[, classes2_logical[i]] <- as.numeric(testing[, classes2_logical[i]])
-      print(classes2_logical[i])
-      print(table(testing[, classes2_logical[i]]))
-      print(class(testing[, classes2_logical[i]]))
+  # testing[, classes2_logical[i]] <- as.numeric(as.character(testing[, classes2_logical[i]]))
+  testing[, classes2_logical[i]] <- as.numeric(testing[, classes2_logical[i]])
+  print(classes2_logical[i])
+  print(table(testing[, classes2_logical[i]]))
+  print(class(testing[, classes2_logical[i]]))
 }
 
 summary(testing)
@@ -149,138 +149,145 @@ save(testing,  file="testing.RData")
 # training_norm <- as.data.frame(lapply(training, normalize))
 
 ################################################################################
+# Extract variables with values not NA in a new DF
+mean0 <- sapply(training, mean)
+# training_mod <- training[, na.omit(names(mean0)[as.integer(as.numeric(!(mean0=="NA"))==1]]
+training_mod <- training[, na.omit(names(mean0)[as.numeric(!(mean0=="NA"))==1])]
+testing_mod <-  testing[, names(training_mod)]
+
+# Include user_name and classe
+training_mod$user_name <- training$user_name
+testing_mod$user_name  <- testing$user_name
+training_mod$classe    <- training$classe
+
+# Complete DF
+temp <- training
+temp$classe <- as.numeric(temp$classe)
+temp2 <- testing
+temp2$problem_id <- NULL
+temp2$classe <- 6
+
+all <- rbind(temp, temp2)
+
+rm(temp)
+rm(temp2)
+
+names(all)
+
+minimum <- all[, c(2,5,6,160)]
+
+minimum$cvtd_timestamp <- as.POSIXct(strptime(minimum$cvtd_timestamp, "%d/%m/%Y %H:%M"))
+minimum <- minimum[order(minimum$user_name, minimum$cvtd_timestamp),]
+
+# Some plots to study data
+plot(minimum$cvtd_timestamp, minimum$classe, col=minimum$user_name, pch=19)
+qplot(cvtd_timestamp, classe, data=all, geom="jitter", colour=factor(user_name), 
+      main="Classe by time", ylab="Classe", xlab="Time")
+
+qplot(cvtd_timestamp, classe, data=all, geom="jitter", colour=factor(user_name), 
+      main="Classe by time", ylab="Classe", xlab="Time") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# Principal componets analysis
+pca.out <- prcomp(training_mod[, -c(57, 58)], scale.=TRUE)
+pca.out
+summary(pca.out)
+biplot(pca.out, scale=0)
+biplot(pca.out, scale=0, cex=.6)
+
+plot(pca.out$x[,1], pca.out$x[,2], xlab="PC1", ylab="PC2")
+
+str(pca.out)
+pca.out$rotation
+names(pca.out)
+
+
+# Correlation
+M_cor <- abs(cor(training[,-c(2, 5, 6, 160)]))
+diag(M_cor) <- 0
+which(M_cor>0.8, arr.ind=T)
+
+new_names <- row.names(which(M_cor>0.8, arr.ind=T))
+new_names
+training_mod3 <- training[,new_names]
+training_mod3$cvtd_timestamp <- training$cvtd_timestamp
+training_mod3$classe         <- training$classe
+
+testing_mod3 <- testing[,new_names]
+testing_mod3$cvtd_timestamp <- testing$cvtd_timestamp
+testing_mod3$classe         <- testing$classe
+################################################################################
 # Models
 
-model1 <- train(classe ~., method="glm", data=training, 
-                preProcess=c("center", "scale"))
-model2 <- train(classe ~., method="rf",
-                data=training, 
-                trControl = trainControl(method="cv"), number=3)
-model3 <- train(classe ~., method="lda", data=training)
-model4 <- train(classe ~., method="nb", data=training)
-model5 <- train(classe ~., method = "C5.0", data=training)
-ctrl <- trainControl(method = "repeatedcv",
-                     repeats = 5)
-model6 <- train(classe ~., method="gbm", data=training, trControl=ctrl)
-names(model6)
-pred6 <- predict(model6, newdata=testing)
-ctrl <- trainControl(method="cv", number=10)
-model7 <- train(classe ~., data=training, method="treebag", trControl=ctrl)
-names(model7)
-pred7 <- predict(model7, newdata=testing)
-# Here, a partial least squares discriminant analysis (PLSDA) model 
-model8  <- train(classe ~., data=training, method="pls",
-                 # Center and scale the predictors for the training
-                 # set and all future samples.
-                 preProc = c("center", "scale"))
-model9  <- train(classe ~., data=training, method="bag")
+# Random Forest
+model30 <- train(classe ~., method="rf", data=training_mod3, 
+                 trControl=trainControl(method="cv"), number=3)
+pred30_train <- predict(model30, training_mod3)
+table(pred30_train, training_mod3$classe)
+pred30 <- predict(model30, testing_mod3)
+print(model30)
+pred30
+confusionMatrix(pred30_train, training_mod3$classe)
 
-model10 <- train(classe ~., data=training, method="svmRadial",
-                 # This pre-processing will be applied to
-                 # these data and new samples too.
-                 preProc=c("center", "scale"),
-                 # Tune over different values of cost
-                 tuneLength=10, trControl=ctrl, metric="ROC")
-
+### Best one
+# Bagging 
 library(ipred)
-model20 <- bagging(classe ~., data=training, nbagg=25)
-pred20_train <- predict(model20, training)
-table(pred20_train, training$classe)
+model31 <- bagging(classe ~., data=training_mod3, nbagg=25)
+pred31_train <- predict(model31, training_mod3)
+table(pred31_train, training_mod3$classe)
+pred31 <- predict(model31, testing_mod3)
+print(model31)
+pred31
+confusionMatrix(pred31_train, training_mod3$classe)
 
-
-
-ctrl <- trainControl(method = "cv", number = 10,
-                     selectionFunction = "oneSE")
-
-# use expand.grid() to create grid of tuning parameters
-grid <- expand.grid(.model = "tree",
-                    .trials = c(1, 5, 10, 15, 20, 25, 30, 35),
-                    .winnow = "FALSE")
-
-# look at the result of expand.grid()
-grid
-
-# customize train() with the control list and grid of parameters 
-set.seed(300)
-m <- train(default ~ ., data = credit, method = "C5.0",
-           metric = "Kappa",
-           trControl = ctrl,
-           tuneGrid = grid)
-m
-
-
-
-
-
-vmbag <- train(default ~ ., data = credit, "bag",
-               trControl = ctrl, bagControl = bagctrl)
-
-# auto-tune a random forest
-grid_rf <- expand.grid(.mtry = c(2, 4, 8, 16))
-
-set.seed(300)
-m_rf <- train(default ~ ., data = credit, method = "rf",
-              metric = "Kappa", trControl = ctrl,
-              tuneGrid = grid_rf)
-
-
-
-rdaGrid = data.frame(gamma = (0:4)/4, lambda = 3/4)
-set.seed(123)
-rdaFit <- train(Class ~ .,
-                data = training,
-                method = "rda",
-                tuneGrid = rdaGrid,
-                trControl = ctrl,
-                metric = "ROC")
-
-gbmTune <- train(x = churnTrain[, predictors],
-                 y = churnTrain$churn,
-                 method = "gbm")
-
-# or, using the formula interface
-gbmTune <- train(churn ~ ., data = churnTrain, method = "gbm")
-
-ctrl <- trainControl(method = "repeatedcv", repeats = 5,
-                     classProbs = TRUE,
-                     summaryFunction = twoClassSummary)
-gbmTune <- train(churn ~ ., data = churnTrain,
-                 method = "gbm",
-                 metric = "ROC",
-                 verbose = FALSE,
-                 trControl = ctrl)
-
-svmTune <- train(churn ~ . , data = churnTrain,
-                 # Tell it to fit a SVM model and tune
-                 # over Cost and the RBF parameter
-                 method = "svmRadial",
-                 # This pre-processing will be applied to
-                 # these data and new samples too.
-                 preProc = c("center", "scale"),
-                 # Tune over different values of cost
-                 tuneLength = 10,
-                 trControl = ctrl,
-                 metric = "ROC")
-
-modFit <- train(eruptions ~ waiting, data=trainFaith, method="lm")
-summary(modFit$finalModel)
-
-modFit <- train(Species ~ ., method="rpart", data=training)
-print(modFit$finalModel)
-
-modFit <- train(Species~ ., data=training, method="rf", prox=TRUE)
-modFit
-
-modFit <- train(wage ~ ., method="gbm", data=training, verbose=FALSE)
-print(modFit)
-
-# Plot the results
-qplot(predict(modFit, testing), wage, data=testing)
-
-
-
+# C5.0
+model32 <- train(classe ~., method = "C5.0", data=training_mod3)
+pred32_train <- predict(model32, training_mod3)
+table(pred32_train, training_mod3$classe)
+pred32 <- predict(model32, testing_mod3)
+print(model32)
+pred32
+confusionMatrix(pred32_train, training_mod3$classe)
 
 ################################################################################
+# Prediction Assignment Submission: Instructions Help
+
+# Please apply the machine learning algorithm you built to each of the 20 test 
+# cases in the testing data set. For more information and instructions on how to 
+# build your model see the prediction assignment writeup. For each test case you 
+# should submit a text file with a single capital letter (A, B, C, D, or E) 
+# corresponding to your prediction for the corresponding problem in the test 
+# data set. You get 1 point for each correct answer. You may submit up to 2 
+# times for each problem. I know it is a lot of files to submit. It may be 
+# helpful to use the following function to create the files. If you have a 
+# character vector with your 20 predictions in order for the 20 problems. So 
+# something like (note these are not the right answers!):
+
+# answers = rep("A", 20)
+answers <- as.character(pred30)
+# B A B A A E D B A A B C B A E E A B B B
+
+# then you can load this function by copying and pasting it into R:
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i], file=filename, quote=FALSE, row.names=FALSE, 
+                col.names=FALSE)
+  }
+}
+
+# then create a folder where you want the files to be written. Set that to be 
+# your working directory and run:
+
+pml_write_files(answers)
+
+# and it will create one file for each submission. Note: if you use this script, 
+# please make sure the files that get written out have one character each with 
+# your prediction for the corresponding problem ID. I have noticed the script 
+# produces strange results if the answers variable is not a character vector. 
+
+
 
 ################################################################################
 # Change to main directory
